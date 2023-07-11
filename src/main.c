f0c734ef16b675106ab4a58758adab587e404cac
Jan Hebbel Aguiar <jan.hebbel@gmail.com>
2023-07-11 21:22:03 +0200

    Drawing to backbuffer.

--- src/main.c
+++ src/main.c
@@ -14,24 +14,39 @@ typedef struct Tag_Window {
 } Window;
 
 static Window window;
+
 static BITMAPINFO bitmap_info;
 static void *bitmap_memory;
-static HBITMAP bitmap_handle;
-static HDC bitmap_device_context;
+static int bitmap_width;
+static int bitmap_height;
+static const int BYTES_PER_PIXEL = 4;
+
+void render_weird_gradient(int xoffset, int yoffset) {
+	int pitch = bitmap_width * BYTES_PER_PIXEL;
+	u8 *row = (u8 *)bitmap_memory;
+	for (int y = 0; y < bitmap_height; ++y) {
+		u32 *pixel = (u32 *)row;
+		for (int x = 0; x < bitmap_width; ++x) {
+			u8 blue  = x + xoffset;
+			u8 green = y + yoffset;
+
+			*pixel++ = (green << 8) | blue;
+		}
+		row += pitch;
+	}
+}
 
 void resize_dib_section() {
-	if (bitmap_handle) {
-		DeleteObject(bitmap_handle);
+	if (bitmap_memory) {
+		VirtualFree(bitmap_memory, 0, MEM_RELEASE);
 	}
-	
-	if (!bitmap_device_context) {
-		// @todo: recreate under certain special circumstances?
-		bitmap_device_context = CreateCompatibleDC(NULL);
-	}
+
+	bitmap_width = window.width;
+	bitmap_height = window.height;
 
 	bitmap_info.bmiHeader.biSize = sizeof(bitmap_info.bmiHeader);
 	bitmap_info.bmiHeader.biWidth = window.width;
-	bitmap_info.bmiHeader.biHeight = window.height;
+	bitmap_info.bmiHeader.biHeight = -window.height; // '-' becaues I want top down dib (origin at top left corner)
 	bitmap_info.bmiHeader.biPlanes = 1;
 	bitmap_info.bmiHeader.biBitCount = 32;
 	bitmap_info.bmiHeader.biCompression = BI_RGB;
@@ -41,19 +56,17 @@ void resize_dib_section() {
 	//bitmap_info.bmiHeader.biClrUsed = 0;
 	//bitmap_info.bmiHeader.biClrImportant = 0;
 
-	bitmap_handle = CreateDIBSection(
-		bitmap_device_context, 
-		&bitmap_info,
-		DIB_RGB_COLORS,
-		&bitmap_memory,
-		NULL, 0);
+	int bitmap_memory_size = BYTES_PER_PIXEL * window.width * window.height;
+	bitmap_memory = VirtualAlloc(NULL, bitmap_memory_size, MEM_COMMIT, PAGE_READWRITE);
+
+	// @todo: probably clear to black
 }
 
 void update_window(HDC device_context, int x, int y, int width, int height) {
 	StretchDIBits(
 		device_context,
-		x, y, width, height, // destination
-		x, y, width, height, // source
+		0, 0, bitmap_width, bitmap_height, // destination
+		0, 0, window.width, window.height, // source
 		bitmap_memory,
 		&bitmap_info,
 		DIB_RGB_COLORS, SRCCOPY);
@@ -89,11 +102,11 @@ LRESULT CALLBACK main_window_callback(HWND w_handle, UINT message, WPARAM wparam
 		case WM_PAINT: {
 			PAINTSTRUCT paint;
 			HDC device_context = BeginPaint(window.handle, &paint);
-			int x = paint.rcPaint.left;
-			int y = paint.rcPaint.top;
-			int width = paint.rcPaint.right - paint.rcPaint.left;
-			int height = paint.rcPaint.bottom - paint.rcPaint.top;
-			update_window(device_context, x, y, width, height);
+			//int x = paint.rcPaint.left;
+			//int y = paint.rcPaint.top;
+			//int width = paint.rcPaint.right - paint.rcPaint.left;
+			//int height = paint.rcPaint.bottom - paint.rcPaint.top;
+			update_window(device_context, 0, 0, window.width, window.height);
 			EndPaint(window.handle, &paint);
 		} break;
 
@@ -114,7 +127,7 @@ LRESULT CALLBACK main_window_callback(HWND w_handle, UINT message, WPARAM wparam
 
 b8 platform_create_window(const char *title, int width, int height) {
 	WNDCLASSA w_class = {0};
-	w_class.style = CS_HREDRAW | CS_VREDRAW;
+	//w_class.style = CS_HREDRAW | CS_VREDRAW; // @todo: is this relevant?
 	w_class.lpfnWndProc = main_window_callback;
 	w_class.hInstance = window.instance;
 	w_class.hCursor = LoadCursor(0, IDC_ARROW);
@@ -219,6 +232,16 @@ int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line,
 		float delta_time = (float)frame_time / 1000.0f;
 
 		platform_process_events();
+
+		static int xoffset = 0;
+		static int yoffset = 0;
+		render_weird_gradient(xoffset, yoffset);
+
+		HDC device_context = GetDC(window.handle);
+		update_window(device_context, 0, 0, window.width, window.height);
+		ReleaseDC(window.handle, device_context);
+
+		++xoffset;
 
 		update();
 		render();
